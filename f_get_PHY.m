c093c2e722750aff4c3f4cb93cd1321b2207b985
Neuhaus <tassianors@gmail.com>
2011-07-12 23:57:36 -0300

    refactor to more generic parameters support

--- f_get_PHY.m
+++ f_get_PHY.m
@@ -1,12 +1,32 @@
-function PHY = f_get_PHY(nd_nn_dim, nn_dim, y, theha_exp)
-N=size(y,1);
-PHY=zeros(nd_nn_dim,nd_nn_dim);
-for i=1:nd_nn_dim
-    for j=1:nd_nn_dim
-        if i>nn_dim && j>nn_dim
+function PHY = f_get_PHY(y, m_dim, m_n_dim, m_texp, m_yu, m_regr)
+%% Gets the PHY based on the model structure
+% y:: output system data [y1,..,yn]
+%% Model
+% m_dim:: Total dimension (num+den dimensions)
+% m_n_dim:: dimension of numerator
+% m_texp::exponential coef from the model
+% m_yu:: one is y, 0 is u. defines witch one of the coef are dependent of y
+%        and u. should be a array with the same size of #m_texp.
+% m_regr:: array whith the regretion dimension (y(k-1) is 1) always
+%          positive values.
+%%
+
+if m_n_dim > m_dim
+    error('m_dim must be bigget than m_n_dim');
+end
+if max(size(m_yu)) < m_dim || max(size(m_regr)) < m_dim || max(size(m_texp)) < m_dim
+    error('invalid parameter dimenstion');
+end
+
+N=max(size(y,1));
+PHY=zeros(m_dim,m_n_dim);
+
+for i=1:m_dim
+    for j=1:m_dim
+        if i>m_n_dim && j>m_n_dim
             aux=0;
             for k=1:N
-                aux=aux+((y(k)^theha_exp(j))* (y(k)^theha_exp(i)));
+                aux=aux+((y(k)^m_texp(j))* (y(k)^m_texp(i)));
             end
             PHY(i,j)=aux;
         end
