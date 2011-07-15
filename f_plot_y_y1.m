9b0cf19273df47f5d76e2d81e034b9bb689fd2b5
Neuhaus <tassianors@gmail.com>
2011-07-15 10:42:17 -0300

    add doc to plot function and some checking of parameters.

--- f_plot_y_y1.m
+++ f_plot_y_y1.m
@@ -1,9 +1,16 @@
 function f_plot_y_y1(vec)
-% funcion util
-N=size(vec,1);
+%% plot the array #vec, where y(k) is X and y(k+1)
+
+N=max(size(vec));
+if N <= 1
+    error('Parameter must be an array!!');
+end
+
 yk=zeros(N-1,1);
 yk1=zeros(N-1,1);
-yk1(1)=vec(1);yk(1)=vec(1);
+
+yk1(1)=vec(1);
+yk(1)=vec(1);
 for k=2:N
     yk(k-1)=vec(k-1);
     yk1(k-1)=vec(k);
