1c50a2f4283a796945139d5d31c5ff3391c14f52
neuhaus <tassianors@gmail.com>
2012-03-28 22:42:54 -0300

    wiener model. finished.

--- vrft_nl/vrft13_nl_wiener.m
+++ vrft_nl/vrft13_nl_wiener.m
@@ -20,7 +20,7 @@ cut=1;
 m=3;
 
 % static non linearity
-b1=1.5;b3=0.5;
+b1=1.5;b3=0.2;
 ga1=0.5;
 gb1=0.9;
 
@@ -64,21 +64,24 @@ end
 % model example 
 %y(k) = (y(k-a1)^b1)*(y(k-c1)^d1)+...+(y(k-an)^bn)*(y(k-cn)^dn)+(y(k-ua1)^ub1)*(u(k-uc1)^ud1)+...+(y(k-uan)^ubn)*(u(k-ucn)^udn)
 %       1+(y(k-an1)^bn1)*(y(k-cn1)^dn1)+...+(y(k-am)^bm)*(y(k-cm)^dm)+(y(k-ua1)^ub1)*(u(k-uc1)^ud1)+...+(y(k-uan)^ubn)*(u(k-ucn)^udn)
-m_rat.n_dim   = 10;
-m_rat.dim     = 10;
+m_rat.n_dim   = rho_size;
+m_rat.dim     = rho_size;
 % to indo do
-m_rat.texp    = [1 2 3 4 5 1 2 3 4 5];
-m_rat.yu      = [2 2 2 2 2 2 2 2 2 2];
-m_rat.regr    = [0 0 0 0 0 1 1 1 1 1];
-% tels if there is some non linearity like (y(k-a)^b)*(y(k-c)^d)
-% r = 3 u = 2 y=1 none =0
-m_rat.yplus_yur = [0 0 0 0 0 0 0 0 0 0];
-% tels the d param
-m_rat.yplus_exp = [0 0 0 0 0 0 0 0 0 0];
-% tels the C param
-m_rat.yplus_regr = [0 0 0 0 0 0 0 0 0 0];
-
+for l=1:m_rat.dim
+    if l<=m_rat.dim/2
+        m_rat.texp(l)=l;
+        m_rat.regr(l)=0;
+    else
+        m_rat.texp(l)=l-m_rat.dim/2;
+        m_rat.regr(l)=1;
+    end
+end
+m_rat.yu      = ones(1,rho_size)*2;
+m_rat.yplus_yur = zeros(1,rho_size);
+m_rat.yplus_exp = zeros(1,rho_size);
+m_rat.yplus_regr = zeros(1,rho_size);
 m_rat.err_enable = true
+
 %% Simulation parameters
 simul=struct('N', N-2, 'nEstimates', 1, 'np', model.noise_std, 'maxError', 0.001, 'l', 100, 'diffConv', .1);
 
@@ -99,13 +102,21 @@ u=zeros(1,N);
 u_hat=zeros(1,N);
 for k=2:N
     e(k)=r(k)-y(k-1);
-    u(k)=u(k-1)+mtheta(1)*e(k)+mtheta(2)*e(k)^2+mtheta(3)*e(k)^3+mtheta(4)*e(k)^4+mtheta(5)*e(k)^5+mtheta(6)*e(k-1)+mtheta(7)*e(k-1)^2+mtheta(8)*e(k-1)^3+mtheta(9)*e(k-1)^4+mtheta(10)*e(k-1)^5;
+    uu=0;
+    for kk=1:rho_size
+        if kk<=rho_size/2
+            uu=uu+mtheta(kk)*e(k);    
+        else
+            uu=uu+mtheta(kk)*e(k-1);                
+        end
+    end
+    u(k)=u(k-1)+uu;
     u_hat(k+1)=gb1*u_hat(k)+ga1*u(k);
     y(k)=(b1*u_hat(k+1)+b3*u_hat(k)^3);
 end
-f_get_vrft_nl_Jmr(M,u(1:N-1), y(2:N))
+f_get_vrft_nl_Jmr(M,r(1:N-1), y(2:N))
 
 
-stairs(y(2:N))
+stairs(y(2:N), '-r')
 hold
 step(M)
